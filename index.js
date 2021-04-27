3d652504425ee32af073451a34be939ac6db7a07
Jesse Coad <jessecoad@gmail.com>
2021-04-27 23:36:56 +1000

    Code added

--- index.js
+++ index.js
@@ -1,13 +1,95 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateMd = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// set the fs.writeFile function to use promises
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Create an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    message: "What is your GitHub user name?",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is your project's title?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Please write a short description of your project.",
+    name: "description",
+  },
+  {
+    type: "list",
+    message: "What license should your project have?",
+    name: "license",
+    choices: [
+      "MIT",
+      "Unlicense",
+      "Apache 2.0",
+      "GNU v3",
+      "BSD 3-Clause",
+      "Mozilla Public License 2.0",
+    ],
+  },
+  {
+    type: "input",
+    message: "What was your motivation for this project?",
+    name: "motivation",
+  },
+  {
+    type: "input",
+    message: "what resources were used for this project?",
+    name: "resources",
+  },
+  {
+    type: "input",
+    message: "What does the user need to know about using the repository?",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Did anyone else contribute to this project?",
+    name: "contributors",
+  },
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+// Ask questions
+const promptUser = () => {
+  return inquirer.prompt(questions);
+};
+
+// Write FILE!
+const writeToFile = (fileName, data) => {
+  return writeFileAsync(fileName, data);
+};
+
+// Lanuch create and write readme
+const init = async () => {
+  try {
+    console.log(
+      "Welcome to the README generator.\nPlease answer the following questions:"
+    );
+
+    const answers = await promptUser();
+    const fileContent = generateMd(answers);
+    await writeToFile("./output/README.md", fileContent);
+    console.log("README.md created in output folder.");
+  } catch (err) {
+    console.error("Error creating README. File not created.");
+    console.log(err);
+  }
+};
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,38 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Grab the needed License
+const licenseBadgeLinks = require("./licenseBadges");
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  // set url for license badge
+  data.licenseBadge = licenseBadgeLinks[data.license];
 
+  // return markdown content
+  return `# ${data.title}
+${data.licenseBadge}
+## Description
+${data.description}
+## Table of Contents
+* [Motivation](#motivation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributors](#contributors)
+* [Sources](#sources)
+* [Questions](#questions)
+## Motivation
+\`
+${data.motivation}
+\`
+## Usage
+${data.usage}
+## License
+This repository is licensed under the ${data.license} license.
+## Contributors
+${data.contributors}
+## Resources
+\`
+${data.resources}
+\`
+## Questions
+Questions about this repository? Please contact me at [${data.email}](mailto:${data.email}). View more of my work in GitHub at [${data.username}](https://github.com/${data.username}) 
 `;
 }
 
